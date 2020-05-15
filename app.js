44c8279b3701eb917ea09ac2542c7541fdee90dc
Andy Sanchez <andysanchez726@gmail.com>
2020-05-15 00:29:00 -0500

    added all the html

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-// const fs = require('fs');
-// const generatePage = require('./src/page-template.js');
+const fs = require('fs');
+const generatePage = require('./src/page-template');
 // const profileDataArgs = process.argv.slice(2, process.argv.length);
 // const [name, github] = profileDataArgs;
 // fs.writeFile('index.html', generatePage(name, github), err => {
@@ -126,5 +126,9 @@ const promptProject = portfolioData => {
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
-        console.log(portfolioData);
+        const pageHTML = generatePage(portfolioData);
+        fs.writeFile('./index.html', pageHTML, err => {
+            if (err) throw new Error(err);
+            console.log('Page created! Check out index.html in this directory to see it!');
+        });
     });
